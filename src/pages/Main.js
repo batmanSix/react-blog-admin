98cfc145cff731e29c0d80abbbd920592fa56101
kangbo <8431331654@qq.com>
2021-02-05 16:06:38 +0800

    提交代码

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -3,7 +3,6 @@ import { BrowserRouter as Router, Route} from "react-router-dom";
 import Login from './Login'
 import AdminIndex from './AdminIndex'
 import routes from "./../router/router"
-import todo from "./todo"
 
 function Main(){
     return (
